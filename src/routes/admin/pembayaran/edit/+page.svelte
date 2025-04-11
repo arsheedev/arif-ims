954e4ac0675593016f06d50ba369456a0b1f29a1
Riyan Fanani <ogickarecka1108@gmail.com>
2025-04-11 18:25:48 +0700

    initial commit

---
+++ src/routes/admin/pembayaran/edit/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import EditPembayaranForm from './EditPembayaranForm.svelte'
+
+	let { data }: { data: PageData } = $props()
+
+	if (data.payment) {
+		data.form.data.name = data.payment.name
+		data.form.data.price = data.payment.price
+	}
+</script>
+
+<div class="flex w-full justify-center">
+	<EditPembayaranForm data={data.form} />
+</div>
---
+++ src/routes/admin/pembayaran/tambah/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import TambahPembayaranForm from './TambahPembayaranForm.svelte'
+
+	let { data }: { data: PageData } = $props()
+</script>
+
+<div class="flex w-full justify-center">
+	<TambahPembayaranForm data={data.form} />
+</div>
