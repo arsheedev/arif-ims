ffc2366abdad44c948430340cafc9194cdab1365
Riyan Fanani <ogickarecka1108@gmail.com>
2025-04-12 17:25:52 +0700

    add: user CRUD functionality

---
+++ src/routes/dashboard/user/edit/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import EditUserForm from './EditUserForm.svelte'
+
+	let { data }: { data: PageData } = $props()
+
+	if (data.user) {
+		data.form.data.name = data.user.name
+		data.form.data.username = data.user.username
+		data.form.data.password = 'asdfasdf'
+		data.form.data.confirmPassword = 'asdfasdf'
+		data.form.data.santriName = data.user.waliSantri?.name || ''
+	}
+</script>
+
+<div class="flex w-full justify-center">
+	<EditUserForm data={data.form} />
+</div>
