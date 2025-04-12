2ab1642c6b4203ee1f4bc2c242ff2202d72a6f1c
Riyan Fanani <ogickarecka1108@gmail.com>
2025-04-12 17:19:58 +0700

    update: register user shcema

--- src/lib/schemas/register-user.ts
+++ src/lib/schemas/register-user.ts
@@ -8,11 +8,7 @@ const RegisterUserSchema = z
 			.min(1, 'Username diperlukan!')
 			.max(32, 'Username tidak boleh lebih dari 32 karakter!'),
 		password: z.string().min(8, 'Password membutuhkan minimal 8 karakter!'),
-		confirmPassword: z.string().min(8, 'Input password ulang dibutuhkan!'),
-		santriName: z
-			.string()
-			.min(1, 'Nama diperlukan!')
-			.max(50, 'Nama tidak boleh lebih dari 50 karakter!')
+		confirmPassword: z.string().min(8, 'Input password ulang dibutuhkan!')
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		path: ['confirmPassword'],
